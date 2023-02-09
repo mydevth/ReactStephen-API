1c8046883239f6cb81915dc6eb6f5cdc6da2a353
mydevth <mydevth@chonburi.biz>
2023-02-09 23:25:22 +0700

    section72

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ### Course : Modern React with Redux
+
 #### Instructor : **_Stephen Grider_**
+
 #### Section05 : **_API_**
-#### Contents : _API_
-#### Tools :  Unsplash API
 
+#### Contents : _API ,Controlled text input_
 
+#### Tools :
 
+- Unsplash API (https://unsplash.com/developers) mydevth
+- axios
 
 #### Result: https://xxxxxx.netlify.app/
--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App(){
-    return <div>App</div>
+import searchImages from "./api";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  const handleSubmit = (term) => {
+    console.log("Do a search with ", term);
+  };
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
-export default App
+export default App;
